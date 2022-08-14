c0d5cd7f464ecc5bf1cfb6fc1cabcbe8716dfaeb
vremes <valtteriremes@outlook.com>
2022-08-14 17:33:29 +0300

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="password_reset_token",
-    version="0.0.3",
+    version="0.0.4",
     author="Valtteri Remes",
     description="Simple and easy to use Python 3 module to generate password reset tokens, based on JWT.",
     long_description=long_description,
