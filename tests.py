630a14a15e7fe0a6e2194ce0d97f87b0d988220d
vremes <valtteriremes@outlook.com>
2022-08-14 17:27:03 +0300

    Update tests.

--- tests.py
+++ tests.py
@@ -1,60 +1,45 @@
 import unittest
+from uuid import uuid4
 from dataclasses import FrozenInstanceError
 
 from password_reset_token import PasswordResetTokenGenerator, PasswordResetToken
 
 SECRET_KEY_FOR_TESTS = 'test'
+GENERATOR = PasswordResetTokenGenerator(SECRET_KEY_FOR_TESTS)
+USER_IDENTIFIER = str(uuid4())
 
 class TestPasswordResetToken(unittest.TestCase):
 
     def test_password_reset_token_generator(self):
-        generator = PasswordResetTokenGenerator(SECRET_KEY_FOR_TESTS)
-
-        token = generator.generate_new_token({
-            "hello": "world"
-        })
+        token = GENERATOR.generate_new_token(USER_IDENTIFIER)
 
         self.assertIsInstance(token, PasswordResetToken)
 
     def test_token_immutability(self):
-        generator = PasswordResetTokenGenerator(SECRET_KEY_FOR_TESTS)
-
-        token = generator.generate_new_token({
-            "hello": "world"
-        })
+        token = GENERATOR.generate_new_token(USER_IDENTIFIER)
 
         with self.assertRaises(FrozenInstanceError):
             token.json_web_token = 'test'
 
     def test_expired_token(self):
-        expired_token = PasswordResetToken('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiZXhwIjoxNjYwMzg5MzQ0fQ.nVskhnaRfMsbZVwJ5O5R6PQoiv9O2QZlZrSRpNQb75s', SECRET_KEY_FOR_TESTS)
+        expired_token = PasswordResetToken('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0ZTcxMDUyYS04YjNjLTQ4NmMtYTNhYi0yZmRlMTBiOTFlMGUiLCJleHAiOjE2NjA0ODYxNDJ9.lnSV4bzW4u0J0fiZIAEAi0V6NEAEKubzQImm3UvLB5Y', SECRET_KEY_FOR_TESTS)
         self.assertTrue(expired_token.is_expired())
 
     def test_fresh_token(self):
-        generator = PasswordResetTokenGenerator(SECRET_KEY_FOR_TESTS)
-
-        token = generator.generate_new_token({
-            "hello": "world"
-        })
+        token = GENERATOR.generate_new_token(USER_IDENTIFIER)
 
         self.assertFalse(token.is_expired())
 
     def test_token_payload(self):
-        generator = PasswordResetTokenGenerator(SECRET_KEY_FOR_TESTS)
-
-        token = generator.generate_new_token({
-            "hello": "world"
-        })
+        token = GENERATOR.generate_new_token(USER_IDENTIFIER)
 
         token_payload = token.get_payload()
 
         self.assertIsInstance(token_payload, dict)
 
-        token_payload_hello = token_payload.get('hello')
+        token_user_identifier = token.get_user_identifier()
 
-        self.assertIsNotNone(token_payload_hello)
-
-        self.assertTrue(token_payload_hello == 'world')
+        self.assertTrue(token_user_identifier == USER_IDENTIFIER)
 
         token_missing_key = token_payload.get('does-not-exist')
 
@@ -81,6 +66,22 @@ class TestPasswordResetToken(unittest.TestCase):
         token_expired = token.is_expired()
 
         self.assertTrue(token_expired)
+
+    def test_token_additional_claims(self):
+        token = GENERATOR.generate_new_token(USER_IDENTIFIER, {
+            "my-custom-claim": "custom-claim-value",
+            "sub": "conflicting-sub"
+        })
+
+        token_payload = token.get_payload()
+
+        self.assertTrue(token_payload.get('sub') == USER_IDENTIFIER)
+        self.assertTrue(token_payload.get('my-custom-claim') == 'custom-claim-value')
+
+    def test_multiple_tokens(self):
+        for uid in range(0, 10):
+            token = GENERATOR.generate_new_token(uid)
+            self.assertTrue(token.get_user_identifier() == uid)
 
 if __name__ == '__main__':
     unittest.main()
