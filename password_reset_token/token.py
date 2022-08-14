e1f7867f5bf7e320b8d7dd0f53b379823fcaf047
vremes <valtteriremes@outlook.com>
2022-08-14 17:26:53 +0300

    Simplify usage.

--- password_reset_token/token.py
+++ password_reset_token/token.py
@@ -1,4 +1,5 @@
 from time import time
+from functools import lru_cache
 from dataclasses import dataclass
 
 from .defaults import JWT_DEFAULT_ALGORITHM, JWT_DEFAULT_EXPIRATION_TIME_SECONDS
@@ -12,6 +13,7 @@ class PasswordResetToken:
     secret: str
     algorithm: str = JWT_DEFAULT_ALGORITHM
 
+    @lru_cache(maxsize=1)
     def get_payload(self) -> dict:
         """Returns the payload for this token as a dict."""
         try:
@@ -29,9 +31,13 @@ class PasswordResetToken:
         except (jwt.exceptions.ExpiredSignatureError, jwt.exceptions.DecodeError):
             return True
 
+    def get_user_identifier(self) -> int | str | None:
+        """Returns user identifier (sub) from this token, this method is a shortcut for `get_payload().get('sub')`."""
+        return self.get_payload().get('sub')
+
     def __decode_jwt(self) -> dict:
         return jwt.decode(self.json_web_token, self.secret, self.algorithm, options={
-            "require": ["exp"]
+            "require": ["exp", "sub"]
         })
 
 class PasswordResetTokenGenerator:
@@ -40,17 +46,20 @@ class PasswordResetTokenGenerator:
         self.secret = secret
         self.algorithm = algorithm
 
-    def generate_new_token(self, payload: dict, exp: int = 0) -> PasswordResetToken:
+    def generate_new_token(self, user_identifier: int | str, additional_claims: dict = {}) -> PasswordResetToken:
         """Generates PasswordResetToken instance for you using given arguments."""
-        exp = int(exp)
-        use_default_expiration_time = exp == 0
+        jwt_payload = {}
+        jwt_payload.update(additional_claims)
 
-        if use_default_expiration_time:
+        exp = additional_claims.get('exp')
+
+        if exp is None:
             exp = int(time()) + JWT_DEFAULT_EXPIRATION_TIME_SECONDS
 
-        payload.update({
+        jwt_payload.update({
+            "sub": user_identifier,
             "exp": exp
         })
 
-        json_web_token = jwt.encode(payload, self.secret, self.algorithm)
+        json_web_token = jwt.encode(jwt_payload, self.secret, self.algorithm)
         return PasswordResetToken(json_web_token, self.secret, self.algorithm)
